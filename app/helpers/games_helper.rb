6a9611f79327faf5344c946db3a61000aad00f84
Nick Meccia <nickmeccia@gmail.com>
2011-12-16 17:34:33 -0600

    Displays all 5 types of victory/loss banners.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,4 +2,34 @@ module GamesHelper
   def position(column_number, row_number)
     column_number.to_i + row_number.to_i * 3
   end
+  
+  def game_over_image(game)
+    if game.draw?
+      return "draw.png"
+    elsif has_computer_player?(game)
+      human_vs_computer_game_over_image(game)
+    else
+      human_vs_human_game_over_image(game)
+    end
+  end
+  
+  def human_vs_computer_game_over_image(game)
+    if game.winner.computer?
+      return "you_lose.png"
+    else
+      return "you_win.png"
+    end
+  end
+  
+  def human_vs_human_game_over_image(game)
+    if game.winner == game.player_one
+      return "p1_wins.png"
+    else
+      return "p2_wins.png"
+    end
+  end
+
+  def has_computer_player?(game)
+    game.player_one.computer? || game.player_two.computer?
+  end
 end
--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -1,4 +1,63 @@
 require 'spec_helper'
 
 describe GamesHelper do
+  context "human vs computer game" do
+    before(:each) do
+      @human = mock("player", :computer? => false)
+      @computer = mock("computer", :computer? => true)
+      @game = mock("game", :player_one => @human, :player_two => @computer, :draw? => false)
+    end
+
+    it "displays the correct message if the human wins" do
+      @game.stub!(:winner).and_return(@human)
+      game_over_image(@game).should == "you_win.png"
+    end
+    
+    it "displays the correct message if the computer wins" do
+      @game.stub!(:winner).and_return(@computer)
+      game_over_image(@game).should == "you_lose.png"
+    end
+    
+    it "has a draw banner" do
+      @game.stub!(:draw?).and_return(true)
+      game_over_image(@game).should == "draw.png"
+    end
+  end
+
+  context "computer vs human game" do
+    before(:each) do
+      @human = mock("player", :computer? => false)
+      @computer = mock("computer", :computer? => true)
+      @game = mock("game", :player_one => @computer, :player_two => @human, :draw? => false)
+    end
+
+    it "displays the correct message if the human wins" do
+      @game.stub!(:winner).and_return(@human)
+      game_over_image(@game).should == "you_win.png"
+    end
+    
+    it "displays the correct message if the computer wins" do
+      @game.stub!(:winner).and_return(@computer)
+      game_over_image(@game).should == "you_lose.png"
+    end
+  end
+
+  context "human vs human game" do
+    before(:each) do
+      @player_one = mock("player", :computer? => false)
+      @player_two = mock("player", :computer? => false)
+      @game = mock("game", :player_one => @player_one, :player_two => @player_two, :draw? => false)
+    end
+    
+    it "displays the correct message if player one wins" do
+      @game.stub!(:winner).and_return(@player_one)
+      game_over_image(@game).should == "p1_wins.png"
+    end
+    
+    it "displays the correct message if player two wins" do
+      @game.stub!(:winner).and_return(@player_two)
+      game_over_image(@game).should == "p2_wins.png"      
+    end
+  end
+
 end
